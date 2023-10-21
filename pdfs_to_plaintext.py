39d6acae2fc6d7adc68d4e7348370a4b2b8a49b5
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2023-10-20 22:51:33 -0400

    tweaking pdf script

--- pdfs_to_plaintext.py
+++ pdfs_to_plaintext.py
@@ -1,13 +1,14 @@
 from pypdf import PdfReader
-import sys
+import glob
 
 def pdf_to_plaintext(pdf_filename: str):
-    print(f"Extracting text from {pdf_filename}")
+    print(f"Extracting text from {pdf_filename} to new file")
     reader = PdfReader(pdf_filename)
     with open(pdf_filename.replace('pdf', 'txt'), 'w') as fp:
         for page in reader.pages:
             fp.write(page.extract_text())
 
 if __name__ == "__main__":
-    for pdf_filename in sys.argv[1:]:
+    pdf_filenames = glob.glob('./data/**/*.pdf', recursive=True)
+    for pdf_filename in pdf_filenames:
         pdf_to_plaintext(pdf_filename)
