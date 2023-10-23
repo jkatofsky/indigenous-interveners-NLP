19720972841eddfc2cd163a895a5daa553d359b7
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2023-10-23 18:11:45 -0400

    tf-idf working (I think)

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ The first step in the process is extracting the raw text from the PDFs so they c
 
 `pdfs_to_plaintext.py` extracts the text from the `.pdf` file passed to its standard input to a `.txt`  file of the same name. To extract all `.pdf`s at once, I passed it `data/**/*.pdf`.
 
+*Note:* this looks to have an issue where some words get occaisonally cut up. I.e. `Canada` will be parsed to `Canad a` in the text file. Not sure why this happens, but I don't think it happens often enough to throw things off. Can investigate later, though.
+
 ## Factum/decision similarity
+
+### Cleaning
 
 I then manually cleaned the `.txt` files, resulting in the `*.clean.txt` files, to only keep the "body" of the document and remove sections that are irrelevant from a semantics perspective. I could have possibly automated this with some extractions in PyPDF, but the dataset is small enough to make manual cleaning viable. After this, there is still a bit of junk, namely inline/footnote citations and sub-section headers, but I don't think that will be too much to throw things off.
 
@@ -20,5 +24,7 @@ Taking in these "clean" documents, `decision_factum_similarity.py` is then the s
 - removal of numbers
 - removal of [stop words](https://en.wikipedia.org/wiki/Stop_word)
 - [lemmatization](https://en.wikipedia.org/wiki/Lemmatization)
+
+### TF-IDF
 
 ...
--- decision_factum_similarity.py
+++ decision_factum_similarity.py
@@ -2,6 +2,8 @@ import sys
 import glob
 import re
 import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def process_document(document: str) -> str:
@@ -20,24 +22,38 @@ def process_document(document: str) -> str:
     return ' '.join(document)
 
 
-def decision_factum_tfidf_cosine_similarity(decision: str, factum: str):
-    pass
+def tf_idf_cosine_similarity(decision: str, factums: dict):
+    print("Computing TF-IDF Cosine Similarity...")
+
+    # TODO: better logs; and verify that everything is checking out
+    vectorizer = TfidfVectorizer(ngram_range=(1, 3))
+    decision_embedding = vectorizer.fit_transform([decision])
+
+    for factum_key in factums:
+        factum_embedding = vectorizer.transform([factums[factum_key]])
+        similarity = cosine_similarity(factum_embedding, decision_embedding)
+        print(f'Factum {factum_key} similarity with decision: {similarity}')
 
 
-def decision_factum_word2vec_cosine_similarity(decision: str, factum: str):
+def word2vec_similarity(decision: str, factums: dict):
+    # TODO
     pass
+
 
 if __name__ == '__main__':
-    dir = sys.argv[1]
+    directories = sys.argv[1:]
 
-    with open(f'{dir}/decision.clean.txt', 'r') as fp:
-        print("Processing decision text")
-        decision = process_document(fp.read())
+    for directory in directories:
+        print(f"Running on directory {directory}")
 
-    factums = dict()
-    for factum_filename in glob.glob(f'{dir}/interveners/*.clean.txt'):
-        with open(factum_filename, 'r') as fp:
-            print(f"Processing {factum_filename} text")
-            factums[factum_filename.split('.')[0]] = process_document(fp.read())
+        with open(f'{directory}/decision.clean.txt', 'r') as fp:
+            print("Processing decision text")
+            decision = process_document(fp.read())
 
-    # TODO: similarity stuff!
+        factums = dict()
+        for factum_filename in glob.glob(f'{directory}/interveners/*.clean.txt'):
+            with open(factum_filename, 'r') as fp:
+                print(f"Processing {factum_filename} text")
+                factums[factum_filename.split('.')[0]] = process_document(fp.read())
+
+        tf_idf_cosine_similarity(decision, factums)
